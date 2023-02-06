667cd08c9080f4005d6a28ffefa02936279090e1
ritujain1208 <ritu@singleinterface.com>
2023-02-06 11:42:46 +0530

    gitprojectadded

---
+++ gitdemo/src/test/java/DemoGit/demogit.java
@@ -0,0 +1,15 @@
+package DemoGit;
+
+import org.testng.annotations.Test;
+
+public class demogit {
+
+	@Test
+	public static void tsetup() {
+		System.out.println("this is gti demo Project **************************");
+		System.out.println("this is gti demo Project **************************");
+
+		System.out.println("this is gti demo Project **************************");
+
+	}
+}
